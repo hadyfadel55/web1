d4898baff4a02087d521affd3557cec6d6141370
hadyfadel55 <hdhodkozm555@gmail.com>
2026-09-19 01:08:53 +0330

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,16 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", function () {
+
+    const x = Math.random() * 400;
+    const y = Math.random() * 200;
+
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+});
+
+const button = document.getElementById("yesBtn");
+
+button.addEventListener("click", function () {
+    alert("nice nigga");
+});
